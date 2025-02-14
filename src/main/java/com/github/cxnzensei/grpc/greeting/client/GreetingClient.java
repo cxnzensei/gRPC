2f3c8e543e4e5addd58ec518e112137811f39701
cxnzensei <nishitpatil3231@gmail.com>
2025-02-14 02:15:49 -0500

    Greet API: Server Streaming

--- src/main/java/com/github/cxnzensei/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/github/cxnzensei/grpc/greeting/client/GreetingClient.java
@@ -1,8 +1,6 @@
 package com.github.cxnzensei.grpc.greeting.client;
 
-import com.proto.dummy.DummyServiceGrpc;
-import com.proto.greet.GreetRequest;
-import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetManyTimesRequest;
 import com.proto.greet.GreetServiceGrpc;
 import com.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
@@ -19,16 +17,25 @@ public class GreetingClient {
                 .build();
 
         System.out.println("Creating stub");
-        // DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
-        // DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.newFutureStub(channel);
 
         GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
 
-        Greeting greeting = Greeting.newBuilder().setFirstName("Nishit").setLastName("Patil").build();
-        GreetRequest greetRequest = GreetRequest.newBuilder().setGreeting(greeting).build();
-        GreetResponse response = greetClient.greet(greetRequest);
+        // Unary
+        //        Greeting greeting = Greeting.newBuilder().setFirstName("Nishit").setLastName("Patil").build();
+        //        GreetRequest greetRequest = GreetRequest.newBuilder().setGreeting(greeting).build();
+        //        GreetResponse response = greetClient.greet(greetRequest);
+        //
+        //        System.out.println(response.getResult());
 
-        System.out.println(response.getResult());
+        // Server Streaming
+        // Prepare the request
+        GreetManyTimesRequest greetManyTimesRequest = GreetManyTimesRequest.newBuilder()
+                        .setGreeting(Greeting.newBuilder().setFirstName("Nishit"))
+                        .build();
+
+        // stream the responses (in a blocking manner)
+        greetClient.greetManyTimes(greetManyTimesRequest)
+                .forEachRemaining(greetManyTimesResponse -> System.out.println(greetManyTimesResponse.getResult()));
 
         // Do something here
 
