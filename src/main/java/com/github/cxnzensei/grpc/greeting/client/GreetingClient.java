d6cd48a2ae37cdeb26e661b31cce16d06e43f723
cxnzensei <nishitpatil3231@gmail.com>
2025-02-19 07:01:27 -0500

    Refactor GreetingClient

--- src/main/java/com/github/cxnzensei/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/github/cxnzensei/grpc/greeting/client/GreetingClient.java
@@ -1,8 +1,6 @@
 package com.github.cxnzensei.grpc.greeting.client;
 
-import com.proto.greet.GreetManyTimesRequest;
-import com.proto.greet.GreetServiceGrpc;
-import com.proto.greet.Greeting;
+import com.proto.greet.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -12,34 +10,45 @@ public class GreetingClient {
 
         System.out.println("Hello I'm a gRPC client");
 
+        GreetingClient main = new GreetingClient();
+        main.run();
+    }
+
+    public void run() {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext() // remove in prod
                 .build();
 
-        System.out.println("Creating stub");
+        // doUnaryCall(channel);
+        doServerStreamingCall(channel);
+
+        System.out.println("Shutting down channel");
+        channel.shutdown();
+    }
+
+    private void doServerStreamingCall(ManagedChannel channel) {
 
         GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
 
-        // Unary
-        //        Greeting greeting = Greeting.newBuilder().setFirstName("Nishit").setLastName("Patil").build();
-        //        GreetRequest greetRequest = GreetRequest.newBuilder().setGreeting(greeting).build();
-        //        GreetResponse response = greetClient.greet(greetRequest);
-        //
-        //        System.out.println(response.getResult());
-
-        // Server Streaming
         // Prepare the request
         GreetManyTimesRequest greetManyTimesRequest = GreetManyTimesRequest.newBuilder()
-                        .setGreeting(Greeting.newBuilder().setFirstName("Nishit"))
-                        .build();
+                .setGreeting(Greeting.newBuilder().setFirstName("Nishit"))
+                .build();
 
         // stream the responses (in a blocking manner)
         greetClient.greetManyTimes(greetManyTimesRequest)
                 .forEachRemaining(greetManyTimesResponse -> System.out.println(greetManyTimesResponse.getResult()));
+    }
 
-        // Do something here
+    private void doUnaryCall(ManagedChannel channel) {
 
-        System.out.println("Shutting down channel");
-        channel.shutdown();
+        // Greet Service client (blocking - synchronous)
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+        Greeting greeting = Greeting.newBuilder().setFirstName("Nishit").setLastName("Patil").build();
+        GreetRequest greetRequest = GreetRequest.newBuilder().setGreeting(greeting).build();
+        GreetResponse response = greetClient.greet(greetRequest);
+
+        System.out.println(response.getResult());
     }
 }
