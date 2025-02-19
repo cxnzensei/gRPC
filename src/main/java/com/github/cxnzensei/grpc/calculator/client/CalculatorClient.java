3b2e629501fbca383d2d240227e2eddf2863be46
cxnzensei <nishitpatil3231@gmail.com>
2025-02-19 07:52:13 -0500

    Client Streaming: Calculator

--- src/main/java/com/github/cxnzensei/grpc/calculator/client/CalculatorClient.java
+++ src/main/java/com/github/cxnzensei/grpc/calculator/client/CalculatorClient.java
@@ -3,8 +3,18 @@ package com.github.cxnzensei.grpc.calculator.client;
 import com.proto.calculator.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class CalculatorClient {
+
+    public static void main(String[] args) {
+
+        CalculatorClient client = new CalculatorClient();
+        client.run();
+    }
 
     public void run() {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50052)
@@ -36,12 +46,38 @@ public class CalculatorClient {
     }
 
     public void doClientStreamingCall(ManagedChannel channel) {
+        CalculatorServiceGrpc.CalculatorServiceStub asyncClient = CalculatorServiceGrpc.newStub(channel);
 
-    }
+        CountDownLatch latch = new CountDownLatch(1);
 
-    public static void main(String[] args) {
+        StreamObserver<ComputeAverageRequest> requestObserver = asyncClient.computeAverage(new StreamObserver<ComputeAverageResponse>() {
+            @Override
+            public void onNext(ComputeAverageResponse computeAverageResponse) {
+                System.out.println("Received a response from the server: " + computeAverageResponse.getAverage());
+            }
 
-        CalculatorClient client = new CalculatorClient();
-        client.run();
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Server has completed sending us data");
+                latch.countDown();
+            }
+        });
+
+        for(int i = 0; i < 100000; i++) {
+            requestObserver.onNext(ComputeAverageRequest.newBuilder().setNumber(i).build());
+        }
+
+        requestObserver.onCompleted();
+        try {
+            latch.await(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
